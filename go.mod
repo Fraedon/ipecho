94a25ab3bea13285e60db9ba9715d291624b01e5
Xavier Brown <secton67@gmail.com>
2019-06-29 15:44:35 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fraedon/ipecho
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var serverAddr = flag.String("a", "localhost:8000", "The address for the server ot listen on")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func echoIP(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the client's connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Don't do anything else — keep the connection allive
+
+	// Echo the client's IP address
+	addr := []byte(r.RemoteAddr)
+	conn.WriteMessage(websocket.TextMessage, addr)
+}
+
+func main() {
+	// Parse the flags
+	flag.Parse()
+
+	// Start the server
+	http.ListenAndServe(*serverAddr, http.HandlerFunc(echoIP))
+}
